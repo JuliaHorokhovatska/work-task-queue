db90fb2d766cdac463bfbc8a190c1bcf0ec04caa
JuliaHorokhovatska <juliahorokhovatska@gmail.com>
2025-07-07 17:44:49 +0300

    Add Market intelligence

---
+++ src/app/dashboard/components/market-intelligence/market-intelligence.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-market-intelligence',
+  imports: [],
+  templateUrl: './market-intelligence.html',
+  styleUrl: './market-intelligence.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class MarketIntelligence {}
--- src/app/dashboard/containers/dashboard/dashboard.ts
+++ src/app/dashboard/containers/dashboard/dashboard.ts
@@ -7,10 +7,11 @@ import { CommonModule } from '@angular/common';
 import { WorkQueue } from '../../components/work-queue/work-queue';
 import { PortfolioGoals } from '../../components/portfolio-goals/portfolio-goals';
 import { QuickActions } from '../../components/quick-actions/quick-actions';
+import { MarketIntelligence } from '../../components/market-intelligence/market-intelligence';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [CommonModule, WorkQueue, PortfolioGoals, QuickActions],
+  imports: [CommonModule, WorkQueue, PortfolioGoals, QuickActions, MarketIntelligence],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
