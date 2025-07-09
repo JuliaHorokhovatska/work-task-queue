12dfdaee89abdfb386db43f157f4ad0c5e82ba60
JuliaHorokhovatska <juliahorokhovatska@gmail.com>
2025-07-09 10:20:57 +0300

    Add Policies

---
+++ src/app/account/components/policies/policies.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-policies',
+  imports: [],
+  templateUrl: './policies.html',
+  styleUrl: './policies.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class Policies {}
--- src/app/account/containers/account/account.ts
+++ src/app/account/containers/account/account.ts
@@ -2,10 +2,11 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 import { GeneralInfo } from '../../components/general-info/general-info';
 import { PerformanceMetrics } from '../../components/performance-metrics/performance-metrics';
+import { Policies } from '../../components/policies/policies';
 
 @Component({
   selector: 'app-account',
-  imports: [GeneralInfo, PerformanceMetrics],
+  imports: [GeneralInfo, PerformanceMetrics, Policies],
   templateUrl: './account.html',
   styleUrl: './account.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
